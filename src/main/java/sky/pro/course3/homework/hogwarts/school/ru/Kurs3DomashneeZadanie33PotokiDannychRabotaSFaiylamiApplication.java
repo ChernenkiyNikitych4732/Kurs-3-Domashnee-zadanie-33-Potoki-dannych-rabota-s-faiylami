7fadf66ddde28476739e0019425b9766dec45aae
Nikita Chernenkiy <chernenkiy.nikitych136@yandex.ru>
2023-10-16 12:52:19 +0200

    Курс 3 Домашнее задание 33 Потоки данных работа с файлами - изначальный комментарий

---
+++ src/main/java/sky/pro/course3/homework/hogwarts/school/ru/Kurs3DomashneeZadanie33PotokiDannychRabotaSFaiylamiApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.course3.homework.hogwarts.school.ru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurs3DomashneeZadanie33PotokiDannychRabotaSFaiylamiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kurs3DomashneeZadanie33PotokiDannychRabotaSFaiylamiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sky/pro/course3/homework/hogwarts/school/ru/Kurs3DomashneeZadanie33PotokiDannychRabotaSFaiylamiApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.course3.homework.hogwarts.school.ru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kurs3DomashneeZadanie33PotokiDannychRabotaSFaiylamiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
